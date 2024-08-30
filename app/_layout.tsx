9cd3e1f0d42f5d534f942357f99e74cecc8735ed
QuitStalin <sanjinknezevic2005@gmail.com>
2024-08-30 09:08:31 -0400

    Dodao prvi stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,23 @@
+// app/_layout.js
+import React from "react";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+
+      <Stack.Screen
+        name="login"
+        options={{ title: "Login", headerShown: false }}
+      />
+
+      <Stack.Screen
+        name="signup"
+        options={{ title: "Sign Up", headerShown: false }}
+      />
+
+      <Stack.Screen name="main" options={{ headerShown: false }} />
     </Stack>
   );
 }
